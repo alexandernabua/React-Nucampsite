2e5885eb63881db76306460418c9d7616f278407
Alexander Nabua <nabua.alexander@gmail.com>
2022-09-18 17:04:39 -0700

    Redux for All

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,7 +1,30 @@
 import { COMMENTS } from '../../app/shared/COMMENTS';
+import { createSlice } from '@reduxjs/toolkit';
 
-export const selectCommentsByCampsiteId = (campsiteId) => {
-    return COMMENTS.filter(
+const initialState= {
+    commentsArray: COMMENTS
+};
+
+const commentsSlice = createSlice({
+    name: 'comments',
+    initialState,
+    reducers: {
+        addComment: (state, action) => {
+            const newComment = {
+                id: state.commentsArray.length +
+                ...action.payload
+            };
+            state.commentsArray.push(newComment);
+        }
+    }
+});
+
+export const commentsReducer = commentsSlice.reducer;
+
+export const { addComment } = commentsSlice.actions;
+
+export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
+    return state.comments.commentsArray.filter(
         (comment) => comment.campsiteId === parseInt(campsiteId)
     );
 };
