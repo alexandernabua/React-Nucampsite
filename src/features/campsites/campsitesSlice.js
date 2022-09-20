1660c621fc47e22bab9135a421d19da6e3addcda
alexandernabua <nabua.alexander@gmail.com>
2022-09-20 11:03:26 -0700

    Async DisplayList

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,6 +1,6 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { mapImageURL } from '../../utils/mapImageURL';
 import { baseUrl } from '../../app/shared/baseUrl';
+import { mapImageURL } from '../../utils/mapImageURL';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchCampsites = createAsyncThunk(
     'campsites/fetchCampsites',
